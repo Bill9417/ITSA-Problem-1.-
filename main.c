4807ada6ba981dab8074231e5558e857e135b81a
Beginer <79035975+Bill9417@users.noreply.github.com>
2024-11-17 00:16:27 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Problem 1.打招呼程式
+//
+//  Created by Shih BoChun on 2024/11/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) {
+    char name[20];
+    scanf("%s", name);
+    printf("Hello %s\n", name);
+    return 0;
+}
+
